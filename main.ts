6fd5458ef30d72220bfeabb7f6d9747018e9717a
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2020-12-21 21:25:53 +0100

    updated speed

--- main.ts
+++ main.ts
@@ -1,21 +1,34 @@
 radio.onReceivedNumber(function (receivedNumber) {
     running = receivedNumber
-    if (running != 1) {
-        music.playMelody("A F E F D G E F ", 463)
+    if (running == 1) {
+        k_Bit.run(DIR.TurnLeft, 50)
         basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
+            . . . . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . . . .
+            `)
+    } else if (running == 2) {
+        k_Bit.run(DIR.TurnRight, 50)
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . . . .
             `)
     } else {
-        basic.showIcon(IconNames.Happy)
-        music.playMelody("- - - - - - - - ", 120)
+        k_Bit.run(DIR.RunForward, 100)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
     }
 })
-let LL = 0
-let LR = 0
 let running = 0
 serial.redirectToUSB()
 k_Bit.Led(COLOR.black)
@@ -24,25 +37,9 @@ strip.showRainbow(1, 360)
 pins.digitalWritePin(DigitalPin.P0, 0)
 radio.setGroup(1)
 basic.forever(function () {
-    if (running == 1) {
-        LR = pins.digitalReadPin(DigitalPin.P12)
-        LL = pins.digitalReadPin(DigitalPin.P13)
-        serial.writeLine("DATA:" + convertToText(LL) + "," + convertToText(LR))
-        serial.writeValue("LL", 0)
-        serial.writeValue("LR", 0)
-        if (LL == 0 && LR == 1) {
-            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 25)
-            k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 25)
-        } else if (LL == 1 && LR == 0) {
-            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 25)
-            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 25)
-        } else if (LL == 1 && LR == 1) {
-            k_Bit.run(DIR.RunForward, 20)
-        } else {
-            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 25)
-            k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 25)
-        }
-        basic.pause(100)
+    if (running != 0) {
+        running = 0
+        basic.pause(200)
     } else {
         k_Bit.carStop()
     }
